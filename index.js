92c8a03411e0b0c1cf8f8fb62ad4afb568869a9d
Léo Martins <leonardomartinsleo@yahoo.com.br>
2022-10-28 08:54:23 -0300

    Correção do Bug ao redimensionar a tela

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ const menuIcon = document.querySelector('.sideMenu img')
 menuIcon.addEventListener('click', function() {
   // quando houver clique no ícone de menu
   menu.classList.toggle('active')
+  
 })
+
+function sideMenuCloser(){
+  let width = window.innerWidth
+  if(width>849){
+    menu.className = 'menu'
+  }
+}
